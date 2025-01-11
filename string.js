b625e751e06d764a8ce1f774303cd175ca79bb6c
Tejac12 <chintalatejavardhan692@gmail.com>
2025-01-11 10:36:17 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,37 @@
+// 1.
+//  let str = "Hello World!";
+// Relace the substring “World!” with “My Dear Friend” ..
+// Write the program for that
+
+let str = "Hello World!";
+let str2 = console.log(str.replace("Hello World!", "My Dear Friend"))
+
+// // 2.
+// let a=”Hungry”;
+// Repeat the “Hungry” for 10 times
+// Write the program for that
+
+let a = " Hungry ";
+console.log(a.repeat(10))
+
+// 3.
+// let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+// Find the last index of the string “fox”
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+console.log(sentence.lastIndexOf("fox"))
+
+
+// 4.let message = "Hello, world!";
+// Find the string starts with “hi”
+// Write code for that
+
+let message = "Hello, world!";
+console.log(message.startsWith("hi"))
+
+
+// 5.let fileName = "document.pdf";
+// Find the string ends with “.pdf”
+// Write code for that
+
+let fileName = "document.pdf";
+console.log(fileName.endsWith(".pdf"))
